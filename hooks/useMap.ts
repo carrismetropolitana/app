efa34c0ed7f927e6bae3b5447d43ee282ef4a6c6
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-08-04 14:54:52 +0100

    Removed unused files and packages

--- hooks/useMap.ts
+++
@@ -1,7 +0,0 @@
-import { MapContext } from '@/contexts/Map.context';
-import { useContext } from 'react';
-
-export function useMap() {
-	const { mapInstance } = useContext(MapContext);
-	return mapInstance;
-}
