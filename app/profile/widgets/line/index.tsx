9862a1ceab956d9b612d44136c11d65bb6b4a30a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-13 10:32:03 +0100

    Add tabler override

---
+++ app/profile/widgets/line/index.tsx
@@ -0,0 +1,53 @@
+/* * */
+
+import { AddFavoriteLineScreen } from '@/components/screens/AddFavoriteLineScreen';
+import { LinesDetailContextProvider } from '@/contexts/LinesDetail.context';
+import { LinesListContextProvider } from '@/contexts/LinesList.context';
+import { StopsDetailContextProvider } from '@/contexts/StopsDetail.context';
+import { useThemeContext } from '@/contexts/Theme.context';
+import { useLocalSearchParams, useNavigation } from 'expo-router';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+/* * */
+
+export default function Page() {
+	//
+
+	//
+	// A. Setup variables
+
+	const params = useLocalSearchParams();
+	const navigation = useNavigation();
+	const themeContext = useThemeContext();
+	const lineID = typeof params.lineId === 'string' ? params.lineId : Array.isArray(params.lineId) ? params.lineId[0] : undefined;
+	const { t } = useTranslation('translation', { keyPrefix: 'addfavoriteline' });
+
+	//
+	// B. Handle actions
+
+	useEffect(() => {
+		navigation.setOptions({
+			headerBackTitle: t('headerTitle'),
+			headerStyle: {
+				backgroundColor: themeContext.theme.mode === 'light' ? themeContext.theme.lightColors?.background : themeContext.theme.darkColors?.background,
+			},
+			headerTitle: '',
+		});
+	}, [navigation]);
+
+	//
+	// C. Render components
+
+	return (
+		<LinesListContextProvider>
+			<LinesDetailContextProvider>
+				<StopsDetailContextProvider>
+					<AddFavoriteLineScreen lineId={lineID} />
+				</StopsDetailContextProvider>
+			</LinesDetailContextProvider>
+		</LinesListContextProvider>
+	);
+
+	//
+}
---
+++ tabler.d.ts
@@ -0,0 +1,4 @@
+declare module '@tabler/icons-react-native' {
+	import * as TablerIcons from 'node_modules/@tabler/icons-react-native/dist/tabler-icons-react-native';
+	export = TablerIcons;
+}
