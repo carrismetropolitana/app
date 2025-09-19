18dd9f6756c2b8dcb2b02ae800ce6211a8fa0786
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-19 12:20:34 +0100

    Organize assets

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './utils/firebaseBgMessaging'; // This is MANADATORY to import before any other stuff
+// import './utils/firebaseBgMessaging'; // This is MANADATORY to import before any other stuff
 import { registerRootComponent } from 'expo';
 
 import App from './App';
