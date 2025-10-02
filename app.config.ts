c037c4858ce972275c0ef594b27c714a483374a0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-10-02 17:48:20 +0100

    Fix wording

--- app.config.ts
+++ app.config.ts
@@ -110,7 +110,7 @@ const config: ExpoConfig = {
 		 * @see https://docs.expo.dev/versions/latest/sdk/location/#configurable-properties
 		 */
 		['expo-location', {
-			locationWhenInUsePermission: 'Allow $(PRODUCT_NAME) to use your location.',
+			locationWhenInUsePermission: 'Utilizamos a sua localização apenas para mostrar as linhas e paragens mais próximos de si.',
 		}],
 
 		/**
