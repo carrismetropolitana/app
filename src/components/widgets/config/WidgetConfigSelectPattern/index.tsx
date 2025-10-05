a1089f352b7897e67c248f00cac80cc16fccc4f3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-10-05 19:42:08 +0100

    Fix

--- src/components/widgets/config/WidgetConfigSelectPattern/index.tsx
+++ src/components/widgets/config/WidgetConfigSelectPattern/index.tsx
@@ -40,7 +40,7 @@ export function WidgetConfigSelectPattern({ availablePatterns, description, onTo
 		if (!availablePatterns?.length) return [];
 		// Prepare patterns list
 		const preparedPatterns = availablePatterns.map(item => ({
-			icon: <LineBadge lineId={item.id} withAlertIcon />,
+			icon: <LineBadge lineId={item.line_id} withAlertIcon />,
 			key: item.id,
 			label: item.headsign,
 			onPress: () => onTogglePatternId(item.id),
