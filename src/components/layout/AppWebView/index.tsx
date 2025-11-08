cf708ccb074d8bdbaa92a71edc4d11d55068f1d0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-11-08 21:18:08 +0000

    Add decelerationRate to webview

--- src/components/layout/AppWebView/index.tsx
+++ src/components/layout/AppWebView/index.tsx
@@ -37,6 +37,7 @@ export function AppWebView({ mediaPlaybackRequiresUserAction = true, url }: AppW
 
 	return (
 		<WebView
+			decelerationRate="normal"
 			mediaPlaybackRequiresUserAction={mediaPlaybackRequiresUserAction}
 			source={{ uri: preparedUrl }}
 			style={{ backgroundColor: systemVariables.background[200] }}
