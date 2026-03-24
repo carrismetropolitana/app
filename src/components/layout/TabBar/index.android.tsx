4ad7c671263ce920f2b6759a43c181aa72e0febf
Afonso Esteves <afonsomgesteves@gmail.com>
2026-03-24 11:34:09 +0000

    feat: implement new TabBar component with updated navigation and icon handling

---
+++ src/components/layout/TabBar/index.android.tsx
@@ -0,0 +1,98 @@
+/* * */
+
+import { useSystemVariables } from '@/theme/global';
+import { IconArrowLoopRight, IconDots, IconMap, IconUserCircle } from '@tabler/icons-react-native';
+import * as Haptics from 'expo-haptics';
+import { Tabs } from 'expo-router';
+import { useTranslation } from 'react-i18next';
+import { View } from 'react-native';
+
+import { useStyles } from './styles';
+
+/* * */
+
+export function TabBar() {
+	//
+
+	//
+	// A. Setup variables
+
+	const styles = useStyles();
+	const systemVariables = useSystemVariables();
+
+	const { t } = useTranslation();
+
+	//
+	// B. Handle actions
+
+	const handleTouchStart = async () => {
+		await Haptics.selectionAsync();
+	};
+
+	//
+	// C. Render components
+
+	return (
+		<Tabs
+			screenOptions={{
+				headerShown: false,
+				sceneStyle: { backgroundColor: systemVariables.background[200] },
+				tabBarActiveTintColor: systemVariables.text[100],
+				tabBarItemStyle: styles.item,
+				tabBarShowLabel: false,
+				tabBarStyle: styles.container,
+			}}
+		>
+			<Tabs.Screen
+				name="(home)"
+				options={{
+					tabBarAccessibilityLabel: t($ => $._app.sitemap['(tabs)/home'].title),
+					tabBarIcon: ({ color, focused }: { color: string, focused: boolean }) => (
+						<View onTouchStart={handleTouchStart} style={[styles.button, focused && styles.buttonIsFocused]}>
+							<IconUserCircle color={focused ? 'black' : color} size={30} />
+						</View>
+					),
+					tabBarLabel: t($ => $._app.sitemap['(tabs)/home'].title),
+				}}
+			/>
+			<Tabs.Screen
+				name="(lines)"
+				options={{
+					tabBarAccessibilityLabel: t($ => $._app.sitemap['(tabs)/lines'].title),
+					tabBarIcon: ({ color, focused }: { color: string, focused: boolean }) => (
+						<View onTouchStart={handleTouchStart} style={[styles.button, focused && styles.buttonIsFocused]}>
+							<IconArrowLoopRight color={focused ? 'black' : color} size={30} />
+						</View>
+					),
+					tabBarLabel: t($ => $._app.sitemap['(tabs)/lines'].title),
+				}}
+			/>
+			<Tabs.Screen
+				name="(stops)"
+				options={{
+					tabBarAccessibilityLabel: t($ => $._app.sitemap['(tabs)/stops'].title),
+					tabBarIcon: ({ color, focused }: { color: string, focused: boolean }) => (
+						<View onTouchStart={handleTouchStart} style={[styles.button, focused && styles.buttonIsFocused]}>
+							<IconMap color={focused ? 'black' : color} size={30} />
+						</View>
+					),
+					tabBarLabel: t($ => $._app.sitemap['(tabs)/stops'].title),
+				}}
+			/>
+			<Tabs.Screen
+				name="more"
+				options={{
+					tabBarAccessibilityLabel: t($ => $._app.sitemap['(tabs)/more'].title),
+					tabBarIcon: ({ color, focused }: { color: string, focused: boolean }) => (
+						<View onTouchStart={handleTouchStart} style={[styles.button, focused && styles.buttonIsFocused]}>
+							<IconDots color={focused ? 'black' : color} size={30} />
+						</View>
+					),
+					tabBarLabel: t($ => $._app.sitemap['(tabs)/more'].title),
+				}}
+			/>
+		</Tabs>
+	);
+
+	//
+}
--- src/components/layout/TabBar/index.tsx
+++ src/components/layout/TabBar/index.tsx
@@ -4,7 +4,6 @@ import { useSystemVariables } from '@/theme/global';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Icon, Label, NativeTabs, VectorIcon } from 'expo-router/unstable-native-tabs';
 import { useTranslation } from 'react-i18next';
-import { DynamicColorIOS } from 'react-native';
 
 /* * */
 
@@ -18,40 +17,27 @@ export function TabBar() {
 	//
 	// C. Render components
 	return (
-		<NativeTabs
-			labelStyle={{
-				// For the text color
-				color: DynamicColorIOS({
-					dark: systemVariables.brand.cm,
-					light: systemVariables.brand.cm,
-				}),
-			}}
-			// For the selected icon color
-			tintColor={DynamicColorIOS({
-				dark: systemVariables.brand.cm,
-				light: systemVariables.brand.cm,
-			})}
-		>
+		<NativeTabs tintColor={systemVariables.text[100]}>
 			<NativeTabs.Trigger name="(home)" options={{ title: t($ => $._app.sitemap['(tabs)/home'].title) }}>
-				<Label hidden>{t($ => $._app.sitemap['(tabs)/home'].title)}</Label>
+				<Label>{t($ => $._app.sitemap['(tabs)/home'].title)}</Label>
 				<Icon
 					androidSrc={<VectorIcon family={MaterialIcons} name="person" />}
 					sf={{ default: 'person.circle', selected: 'person.circle.fill' }}
 				/>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="(lines)" options={{ title: t($ => $._app.sitemap['(tabs)/lines'].title) }}>
-				<Label hidden>{t($ => $._app.sitemap['(tabs)/lines'].title)}</Label>
+				<Label>{t($ => $._app.sitemap['(tabs)/lines'].title)}</Label>
 				<Icon androidSrc={<VectorIcon family={MaterialIcons} name="swap-calls" />} sf="arrow.trianglehead.swap" />
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="(stops)" options={{ title: t($ => $._app.sitemap['(tabs)/stops'].title) }}>
-				<Label hidden>{t($ => $._app.sitemap['(tabs)/stops'].title)}</Label>
+				<Label>{t($ => $._app.sitemap['(tabs)/stops'].title)}</Label>
 				<Icon
 					androidSrc={<VectorIcon family={MaterialIcons} name="map" />}
 					sf={{ default: 'map', selected: 'map.fill' }}
 				/>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="more" options={{ title: t($ => $._app.sitemap['(tabs)/more'].title) }}>
-				<Label hidden>{t($ => $._app.sitemap['(tabs)/more'].title)}</Label>
+				<Label>{t($ => $._app.sitemap['(tabs)/more'].title)}</Label>
 				<Icon
 					androidSrc={<VectorIcon family={MaterialIcons} name="more-horiz" />}
 					sf={{ default: 'ellipsis.circle', selected: 'ellipsis.circle.fill' }}
